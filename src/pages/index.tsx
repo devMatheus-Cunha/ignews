9bf014aef34faa2b249b21fd8750af2c289467cd
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-16 09:35:52 -0300

    applying and configuring scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import style from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-  <h1>Hello Word</h1>
-  )
+    <h1 className={style.title}>
+      Hello Word
+    </h1>
+  );
 }
