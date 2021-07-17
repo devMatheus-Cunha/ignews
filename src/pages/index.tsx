f23e01918e01c3ef8bf74a7ce14e549ba8f7abbc
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-17 11:45:35 -0300

    feat: applying dynamic title to each page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from "next/head"
+
+// style
 import style from "../styles/home.module.scss";
 
 export default function Home() {
 	return (
-		<h1 className={style.title}>
-			Hello Word
-		</h1>
+		<>
+			<Head>
+				<title>Inicio | ig.news</title>
+			</Head>
+			<h1 className={style.title}>
+				Hello Word
+			</h1>
+		</>
 	);
 }
