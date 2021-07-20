45e87e14997043da66a237702b027d8e32050320
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-20 10:36:48 -0300

    feat: component home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
 
+import styles from "../styles/home.module.scss"
+
 // -------------------------------------------------
 // Export Function
 // -------------------------------------------------
@@ -10,11 +12,27 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Inicio | ig.news</title>
+				<title>Home | ig.news</title>
 			</Head>
-			<h1>
-				Hello Word
-			</h1>
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>Hey, welcome</span>
+					<h1>
+						News about the
+						{" "}
+						<span>React</span>
+						{" "}
+						world.
+					</h1>
+					<p>
+						Get acess to all the publications
+						{" "}
+						<br />
+						<span>for $9.90 month</span>
+					</p>
+				</section>
+				<img src="/images/avatar.svg" alt="Girl coding" />
+			</main>
 		</>
 	);
 }
