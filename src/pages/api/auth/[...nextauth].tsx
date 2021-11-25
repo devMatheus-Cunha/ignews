02b620b5b031be08d7f6d03599533d2c4827fc81
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-11-25 16:13:43 -0300

    feat: create collection in  faunadb

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+// fauna
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
 	providers: [
@@ -9,4 +13,17 @@ export default NextAuth({
 			scope: "read:user",
 		}),
 	],
-})
+	jwt: {
+		signingKey: process.env.SIGNING_KEY,
+	},
+	callbacks: {
+		async signIn({ email }) {
+			try {
+				await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+				return true;
+			} catch {
+				return false;
+			}
+		},
+	},
+});
