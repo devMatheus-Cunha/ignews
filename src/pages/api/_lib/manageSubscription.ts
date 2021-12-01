f5d809646d8242b20f19bc7e8fef314f4594a8fc
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-01 12:53:05 -0300

    feat: updated faunaDB with stripe canceled and updated

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,34 +1,54 @@
-import { query } from "faunadb";
-import { stripe } from "../../../services/stripe";
+import { query as q } from "faunadb"
 import { fauna } from "../../../services/fauna";
+import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
-	customerId: string,
 	subscriptionId: string,
+	customerId: string,
+	createAction = false,
 ) {
 	const userRef = await fauna.query(
-		query.Select(
+		q.Select(
 			"ref",
-			query.Get(
-				query.Match(query.Index("user_by_stripe_customer_id"), customerId),
+			q.Get(
+				q.Match(
+					q.Index("user_by_stripe_customer_id"),
+					customerId,
+				),
 			),
 		),
-	);
+	)
 
-	const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+	const subscription = await stripe.subscriptions.retrieve(subscriptionId)
 
 	const subscriptionData = {
 		id: subscription.id,
 		userId: userRef,
 		status: subscription.status,
 		price_id: subscription.items.data[0].price.id,
-	};
+	}
 
-	await fauna.query(
+	if (createAction) {
 		await fauna.query(
-			query.Create(query.Collection("subscriptions"), {
-				data: subscriptionData,
-			}),
-		),
-	);
+			q.Create(
+				q.Collection("subscriptions"),
+				{ data: subscriptionData },
+			),
+		)
+	} else {
+		await fauna.query(
+			q.Replace(
+				q.Select(
+					"ref",
+					q.Get(
+						q.Match(
+							q.Index("subscription_by_id"),
+							subscriptionId,
+						),
+					),
+				),
+				{ data: subscriptionData },
+			),
+		)
+	}
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,31 +1,33 @@
-// packages
 import { NextApiRequest, NextApiResponse } from "next";
-import { Readable } from "stream";
 
-// stripe
+import { Readable } from "stream"
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
-
-// lib
 import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
 	const chunks = [];
 
 	for await (const chunk of readable) {
-		chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+		chunks.push(
+			typeof chunk === "string" ? Buffer.from(chunk) : chunk,
+		)
 	}
 
-	return Buffer.concat(chunks);
+	return Buffer.concat(chunks)
 }
 
 export const config = {
 	api: {
 		bodyParser: false,
 	},
-};
+}
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+	"checkout.session.completed",
+	"customer.subscription.updated",
+	"customer.subscription.deleted",
+])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === "POST") {
@@ -35,13 +37,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		let event: Stripe.Event;
 
 		try {
-			event = stripe.webhooks.constructEvent(
-				buf,
-				secret,
-				process.env.STRIPE_WEBHOOK_SECRET,
-			);
+			event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
 		} catch (err) {
-			return res.status(400).send(`Webhook error: ${err.message}`);
+			return res.status(400).send(`Webhook error: ${err.message}`)
 		}
 
 		const { type } = event;
@@ -49,27 +47,40 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		if (relevantEvents.has(type)) {
 			try {
 				switch (type) {
-				case "checkout.session.completed":
-					const checkoutSession = event.data.object as Stripe.Checkout.Session
+				case "customer.subscription.updated":
+				case "customer.subscription.deleted":
+					const subscription = event.data.object as Stripe.Subscription;
+
 					await saveSubscription(
-						checkoutSession.customer.toString(),
-						checkoutSession.subscription.toString(),
+						subscription.id,
+						subscription.customer.toString(),
+						false,
 					)
+
+					break;
+
+				case "checkout.session.completed":
+
+					const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+					await saveSubscription(
+						checkoutSession.subscription.toString(),
+						checkoutSession.customer.toString(),
+						true,
+					)
+
 					break;
 				default:
-					throw new Error("Unchadled event");
-					break;
+					throw new Error("Unhandled event")
 				}
-			} catch (error) {
-				return res.json({ error: "webhook handler failed" })
+			} catch (err) {
+				return res.json({ error: "Webhook handler failed" })
 			}
 		}
 
-		res.json({ received: true });
+		res.json({ received: true })
 	} else {
 		res.setHeader("Allow", "POST");
 		res.status(405).end("Method not allowed");
 	}
-
-	return true;
-};
+}
