806287e7dca02fbf984e316e99b241494a9cceca
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-12-02 16:43:37 -0300

    feat: validate if user is logged and with active subscribe

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -17,24 +17,53 @@ export default NextAuth({
 		}),
 	],
 	callbacks: {
+		async session(session) {
+			try {
+				const userActiveSubscription = await fauna.query(
+					query.Get(
+						query.Intersection([
+							query.Match(
+								query.Index("subscription_by_user_ref"),
+								query.Select(
+									"ref",
+									query.Get(
+										query.Match(
+											query.Index("user_by_email"),
+											query.Casefold(session.user.email),
+										),
+									),
+								),
+							),
+							query.Match(
+								query.Index("subscription_by_status"),
+								"active",
+							),
+						]),
+					),
+				)
+				return {
+					...session,
+					activeSubscription: userActiveSubscription,
+				}
+			} catch (err) {
+				return {
+					...session,
+					activeSubscription: null,
+				}
+			}
+		},
 		async signIn({ email }) {
 			try {
 				await fauna.query(
 					query.If(
 						query.Not(
 							query.Exists(
-								query.Match(
-									query.Index("user_by_email"),
-									query.Casefold(email),
-								),
+								query.Match(query.Index("user_by_email"), query.Casefold(email)),
 							),
 						),
 						query.Create(query.Collection("users"), { data: { email } }),
 						query.Get(
-							query.Match(
-								query.Index("user_by_email"),
-								query.Casefold(email),
-							),
+							query.Match(query.Index("user_by_email"), query.Casefold(email)),
 						),
 					),
 				);
