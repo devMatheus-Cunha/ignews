cf2c2a79909d33cc929dd1ef1aa19242c5695722
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-01-10 18:08:32 -0300

    feat: test renders correctly Home page

---
+++ src/tests/pages/Homer.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+import { stripe } from "../../services/stripe";
+import Home, { getStaticProps } from "../../pages";
+
+jest.mock("next/router");
+
+jest.mock("next-auth/client", () => {
+  return {
+    useSession: () => [null, false],
+  };
+});
+
+jest.mock("../../services/stripe");
+
+describe("Home page", () => {
+  it("renders correctly", () => {
+    render(<Home product={{ priceId: "fake-price-ide", amount: "R$10,00" }} />);
+
+    expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+});
