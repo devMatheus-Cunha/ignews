ff4a2e507e3d02ad3e5ababa4e7597201ece24de
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-01-10 10:16:51 -0300

    style: new formated code

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,7 +1,13 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+
+// utils
 import { mocked } from "ts-jest/utils";
+
+// next
 import { signIn, useSession } from "next-auth/client";
 import { useRouter } from "next/router";
+
+// components
 import { SubscribeButton } from ".";
 
 jest.mock("next-auth/client");
@@ -21,7 +27,6 @@ describe("SubscribeButton component", () => {
 
   it("redirects user to sign in when not authenticated", () => {
     const signInMocked = mocked(signIn);
-
     const useSessionMocked = mocked(useSession);
 
     useSessionMocked.mockReturnValueOnce([null, false]);
@@ -37,9 +42,7 @@ describe("SubscribeButton component", () => {
 
   it("redirects to posts when user already has a subscription", () => {
     const useRouterMocked = mocked(useRouter);
-
     const useSessionMocked = mocked(useSession);
-
     const pushMock = jest.fn();
 
     useSessionMocked.mockReturnValueOnce([
