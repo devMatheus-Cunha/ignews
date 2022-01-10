446450b5cc0063a624d11c180a207559ecef880a
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-01-10 10:15:31 -0300

    feat: test redirects to posts when user already has a subscription of SubscribeButton component

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,32 +1,66 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { mocked } from "ts-jest/utils";
-import { signIn } from "next-auth/client";
+import { signIn, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 import { SubscribeButton } from ".";
 
-jest.mock("next-auth/client", () => {
-  return {
-    useSession() {
-      return [null, false];
-    },
-    signIn: jest.fn()
-  };
-});
+jest.mock("next-auth/client");
+
+jest.mock("next/router");
 
 describe("SubscribeButton component", () => {
   it("renders correctly", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
     render(<SubscribeButton />);
 
     expect(screen.getByText("Subscribe now")).toBeInTheDocument();
   });
 
   it("redirects user to sign in when not authenticated", () => {
-    const signInMoked = mocked(signIn)
+    const signInMocked = mocked(signIn);
+
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
 
     render(<SubscribeButton />);
-    
-    const subscribeButton = screen.getByText("Subscribe now")
-    fireEvent.click(subscribeButton)
 
-    expect(signInMoked).toHaveBeenCalled()
+    const subscribeButton = screen.getByText("Subscribe now");
+
+    fireEvent.click(subscribeButton);
+
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it("redirects to posts when user already has a subscription", () => {
+    const useRouterMocked = mocked(useRouter);
+
+    const useSessionMocked = mocked(useSession);
+
+    const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: { name: "John Doe", email: "john.doe@example.com" },
+        expires: "fake-expiration",
+        activeSubscription: "fake-active-subscription",
+      },
+      false,
+    ]);
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText("Subscribe now");
+
+    fireEvent.click(subscribeButton);
+
+    expect(pushMock).toHaveBeenCalledWith("/posts");
   });
 });
