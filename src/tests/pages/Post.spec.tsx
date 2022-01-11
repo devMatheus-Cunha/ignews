adbc0d15ea7a2d0492e59d5d57bd02a5cdbf5388
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-01-11 10:46:23 -0300

    feat: test loads initial data Post page

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -40,4 +40,44 @@ describe("Posts page", () => {
       })
     )
   })
+
+  it('loads initial data', async () => {
+    const getSessionMocked = mocked(getSession)
+    const getPrismicClientMocked = mocked(getPrismicClient)
+
+    getSessionMocked.mockResolvedValueOnce({
+      activeSubscription: 'fake-active-subscription'
+    } as any)
+    
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [
+            {type: 'heading', text: 'My New Post'}
+          ],
+          content: [
+            {type: 'paragraph', text: 'Post excerpt'}
+          ],
+        },
+        last_publication_date: '04-01-2021',
+      })
+    } as any)
+
+    const response = await getServerSideProps({
+      params: { slug: 'my-new-post'}
+    } as any)
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'my-new-post',
+            title: 'My New Post',
+            content: '<p>Post excerpt</p>',
+            updatedAt: '01 de abril de 2021'
+          }
+        }
+      })
+    )
+  })
 });
