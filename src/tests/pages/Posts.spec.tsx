c52c31d6e12af94a69dacd67be60691ab7d098e7
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-01-11 08:27:03 -0300

    feat: test loads initial data Posts page

--- src/tests/pages/Posts.spec.tsx
+++ src/tests/pages/Posts.spec.tsx
@@ -26,4 +26,51 @@ describe("Posts page", () => {
 
     expect(screen.getByText("My new post")).toBeInTheDocument();
   });
+
+  it("loads initial data", async () => {
+    const getPrismicClientMoked = mocked(getPrismicClient);
+
+    getPrismicClientMoked.mockReturnValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: "my-new-post",
+            data: {
+              title: [
+                {
+                  type: "heading",
+                  text: "My new post",
+                },
+              ],
+              content: [
+                {
+                  type: "paragraph",
+                  text: "Post excerpt",
+                },
+              ],
+            },
+            last_publication_date: "04-01-2021",
+          },
+        ],
+      }),
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        // Verifica se existe no minímo essas propriedades no objeto
+        props: {
+          posts: [
+            {
+              slug: "my-new-post",
+              title: "My new post",
+              excerpt: "Post excerpt",
+              updatedAt: "01 de abril de 2021",
+            },
+          ],
+        },
+      })
+    );
+  });
 });
